f8e54f78581cd836229f0f06c1297cdba0c63ab1
Oarkflow <info@oarkflow.com>
2023-12-12 18:56:43 +0545

    Initial Commit

--- examples/main.go
+++ examples/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-
+	
 	"github.com/oarkflow/phonenumbers"
 )
 
 func main() {
-
+	err := phonenumbers.LoadNetworks()
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println(phonenumbers.Verify("9856034616", "NP"))
 }
